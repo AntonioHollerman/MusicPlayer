b5d2e02179bcb51847b6bc037ed6b035679cccbb
AntonioHollerman <toniohollerProf@gmail.com>
2024-02-28 07:52:27 -0500

    initialize structure for Playlist.java

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,2 +1,57 @@
 public class Playlist {
+    private Song[] songArr;
+    private int curSongIndex;
+    private String playlistName;
+    private String imgPath;
+    private final int playlistId;
+
+    public Playlist(String playlistName, String imgPath, int playlistId) {
+        this.playlistName = playlistName;
+        this.imgPath = imgPath;
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+    public Song[] getSongArr(){
+        return songArr;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+    public void shufflePlaylist(){
+
+    }
+    public Song getCurSong(){
+        return null;
+    }
+    public Song getNextSong(){
+        return null;
+    }
+    public Song getPrevSong(){
+        return null;
+    }
+    public void addSong(Song newSong){
+
+    }
+    public void startPlaylist(int songId){
+
+    }
+    public void startPlaylist(){
+
+    }
 }
