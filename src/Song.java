35fa0d45770aba65aac35145a814941eec408eb6
Antonio Hollerman <toniohollerprof@gmail.com>
2024-02-27 22:00:28 -0500

    initialize structure for Song

--- src/Song.java
+++ src/Song.java
@@ -1,2 +1,52 @@
 public class Song {
+    private String songPath;
+    private String imgPath;
+    private final int id;
+    private String title;
+    private boolean isPlaying;
+
+    public Song(String songPath, String imgPath, int id, String title) {
+        this.songPath = songPath;
+        this.imgPath = imgPath;
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getSongPath() {
+        return songPath;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
+    public void setSongPath(String songPath) {
+        this.songPath = songPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void play(){
+
+    }
+    public void pause(){
+
+    }
 }
