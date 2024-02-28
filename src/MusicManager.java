fa8d80bb1527ddbd1376d52c27ad8cd0feaebb09
AntonioHollerman <toniohollerProf@gmail.com>
2024-02-28 14:57:44 -0500

    Initialize structure for MusicManager.java

---
+++ src/MusicManager.java
@@ -0,0 +1,21 @@
+public class MusicManager {
+    private Folder curFolder;
+    public MusicManager(){
+
+    }
+    public void swapFolder(){
+
+    }
+    public void selectPlaylist(){
+
+    }
+    public void exitPlaylist(){
+
+    }
+    public void changeFolderName(){
+
+    }
+    public void changePlaylistName(){
+
+    }
+}
