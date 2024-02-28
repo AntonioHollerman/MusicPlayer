f3d29e0766557b9fd47430cf28a8d58dcd8066a8
AntonioHollerman <toniohollerProf@gmail.com>
2024-02-28 14:40:09 -0500

    Initialize structure for DbManager.java

---
+++ src/DbManager.java
@@ -0,0 +1,47 @@
+public class DbManager {
+    public Folder[] getFolders(){
+        return null;
+    }
+    public Folder[] getFolders(int folderId){
+        return null;
+    }
+    public Playlist[] getPlaylists(int folderId){
+        return null;
+    }
+    public Song[] getSongs(int playlistId){
+        return null;
+    }
+    public String getImgPath(int imgId){
+        return null;
+    }
+    public int getNextSongId(){
+        return 0;
+    }
+    public int getNextImgId(){
+        return 0;
+    }
+    public void addSongToPlaylist(int songId, int playlistId){
+
+    }
+    public void insertNewSong(Song newSong){
+
+    }
+    public void insertNewPlaylist(Playlist newPlaylist){
+
+    }
+    public void insertNewFolder(Folder newFolder){
+
+    }
+    public void deleteSongFromPlaylist(int playlistId, int songId){
+
+    }
+    public void deleteSong(int songId){
+
+    }
+    public void deletePlaylist(int playlistId){
+
+    }
+    public void deleteFolder(int folderId){
+
+    }
+}
