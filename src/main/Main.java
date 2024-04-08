1e0c1db3bcc547ca231718af7e76389e0f17fa9d
AntonioHollerman <toniohollerProf@gmail.com>
2024-04-08 08:27:04 -0400

    Finish commenting classes in the main package.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
+/**
+ * The Main class serves as the entry point for the application.
+ * It contains the main method that is executed when the program starts.
+ */
 public class Main {
     public static void main(String[] args) {
 
--- src/main/MasterWindow.java
+++ src/main/MasterWindow.java
@@ -1,4 +1,7 @@
 package main;
 
+/**
+ * The window that hosts the panels created in GUI
+ */
 public class MasterWindow {
 }
