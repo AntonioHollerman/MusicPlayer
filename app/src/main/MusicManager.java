74cb960c7c3073c2c8de5f54a22c24301bceefe5
Antonio Hollerman <toniohollerprof@gmail.com>
2024-07-30 10:28:32 -0400

    populate MusicManager methods

--- app/src/main/MusicManager.java
+++ app/src/main/MusicManager.java
@@ -1,40 +1,49 @@
 package main;
 
+import api.DbRowToStructureApi;
+import content.Content;
+import db.DbManager;
+import db.DbService;
 import structures.Folder;
 import structures.Playlist;
 
 public class MusicManager {
     private Folder curFolder;
+    private static final DbManager dbConn = DbService.DB_CONNECTION;
 
     /**
      * Initialize and organizes folders, playlist, and songs
      */
     public MusicManager(){
-
+        this.curFolder = DbRowToStructureApi.toFolder(dbConn.getRootFolder());
     }
 
     /**
      * Changes working folder to a direct sub or parent folder
      */
     public void swapFolder(int folderId){
-
+        for (Content content : curFolder.getContent()){
+            if (content instanceof Folder folder && folder.getId() == folderId){
+                curFolder = folder;
+                return;
+            }
+        }
+        throw new RuntimeException("Folder being swapped to does not exist");
     }
 
-    /**
-     * Changes the name of the given folder.
-     *
-     * @param folder the folder whose name needs to be changed
-     */
-    public void changeFolderName(Folder folder){
-
+    public void swapPrevFolder(){
+        if (curFolder.getParentFolder() == null){
+            throw new RuntimeException("Folder does not have a parent");
+        }
+        curFolder = curFolder.getParentFolder();
     }
 
-    /**
-     * Changes the name of the given playlist.
-     *
-     * @param playlist the playlist whose name needs to be changed
-     */
-    public void changePlaylistName(Playlist playlist){
-
+    public Playlist getPlaylist(int playlistId){
+        for (Content content : curFolder.getContent()){
+            if (content instanceof Playlist playlist && playlist.getId() == playlistId){
+                return playlist;
+            }
+        }
+        throw new RuntimeException("Folder does not have wanted playlist");
     }
 }
