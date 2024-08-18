b43aa939eb2d7871a79d53e34f98238790bd0b0d
Antonio Hollerman <toniohollerprof@gmail.com>
2024-08-18 13:31:09 -0400

    Convert gui structure to the JavaFX preset

---
+++ gui/src/main/java/ui/master/MasterWindow.java
@@ -0,0 +1,4 @@
+package ui.master;
+
+public class MasterWindow {
+}
--- gui/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module gui {
-    requires music;
-    exports ui.add;
-    exports ui.content;
-    exports ui.edit;
-    exports ui.player;
-}
