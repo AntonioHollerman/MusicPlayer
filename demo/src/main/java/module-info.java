4553054ae7e935d1a054f8a7d019f4c04f686564
Antonio Hollerman <toniohollerprof@gmail.com>
2025-09-10 01:00:07 -0400

    fix integration of graphics with the rest of the project

--- demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module gui.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens gui.demo to javafx.fxml;
-    exports gui.demo;
-}
---
+++ graphics/src/module-info.java
@@ -0,0 +1,9 @@
+module graphics {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens application to javafx.fxml;
+    opens controllers to javafx.fxml;
+    exports application;
+}
